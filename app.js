0d2196f6027209ee6082b00879703e02c94b2bda
AlanaMart <115120017+AlanaMart@users.noreply.github.com>
2024-12-03 14:57:18 -0300

    Add files via upload
    v1

---
+++ app.js
@@ -0,0 +1,10 @@
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+            console.log('Service Worker registrado no domínio:', registration.scope);
+        })
+        .catch((error) => {
+            console.error('O registro do Service Worker falhou:', error);
+        });
+}
